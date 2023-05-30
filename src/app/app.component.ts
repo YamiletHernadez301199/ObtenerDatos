9e022110a0729578e1c80485a747671bb57d2d2d
Yamilet <yamilet.hdz.301199@gmail.com>
2023-05-30 12:47:30 -0600

    Trabajo Terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { articulo } from './interfaces/articulos.interface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'obtenerDatos';
+  public articulos: articulo[] = [];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    let url = "http://localhost/productos/";
+    this.http.get<articulo[]>(url)
+    .subscribe(
+      resultado=>{
+        this.articulos = resultado;
+        console.log(this.articulos);
+      }
+    )
+  }
 }
